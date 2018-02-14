1a1819391fe4f74f637e79e2f6bfdb2182faba41
nithinreddy103 <32595638+nithinreddy103@users.noreply.github.com>
2018-02-14 19:29:13 +0530

    Update single- mainobject.scala

--- single- mainobject.scala
+++ single- mainobject.scala
@@ -1,1 +1,16 @@
 
+class Employer{
+  var salary:Float = 32000
+}
+
+class Programmer extends Employer{
+  var bonus:Int = 15000
+  println("Salary = "+salary)
+  println("Variable pay = "+bonus)
+}
+
+object MainObject{
+  def main(args:Array[String]){
+    new Programmer()
+  }
+}
